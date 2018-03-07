d0eb22335ea3eb79691a58667e29d386d63ed3ad
dominicespiritu <gavegen@yahoo.com>
2018-03-07 20:07:56 +0800

    updates

--- app1/src/app/recipe/recipe-list/recipe-list.component.ts
+++ app1/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe("test name","test desc","https://www.edgarsmission.org.au/wp-content/uploads/2017/06/GoogleDoodle.png"),
+    new Recipe("test name 2","test desc 2","https://timedotcom.files.wordpress.com/2017/10/celebrating-the-studio-for-electronic-music-4914189628866560-3-2x.png")
+  ];
   constructor() { }
 
   ngOnInit() {
